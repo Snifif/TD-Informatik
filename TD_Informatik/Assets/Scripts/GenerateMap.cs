59fbc8fc5ab41465b71005a738f3748909b9f7c4
Snifif <66800395+Snifif@users.noreply.github.com>
2020-12-01 17:43:21 +0100

    Projekt erstellt

---
+++ TD_Informatik/Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public GameObject Node;
+
+    private int MapBREITE = 9;
+    private int MapHoehe = 9;
+
+    private void MapGenerate()
+    {
+        for (float z = 0; z < 2 * MapBREITE; z = z + 2)
+        {
+            for (int x = 0; x < 2 * MapHoehe; x = x + 2)
+            {
+                GameObject newNode = Instantiate(Node);
+                newNode.transform.position = new Vector3(x, 0, z);
+
+            }
+        }
+    }
+    private void Start()
+    {
+        MapGenerate();
+    }
+}
