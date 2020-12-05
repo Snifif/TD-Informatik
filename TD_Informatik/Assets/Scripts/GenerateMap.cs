5224549cc3942e82ff519d1c02a561dec5a79a15
Snifif <66800395+Snifif@users.noreply.github.com>
2020-12-05 17:18:39 +0100

    Update GenerateMap.cs

--- TD_Informatik/Assets/Scripts/GenerateMap.cs
+++ TD_Informatik/Assets/Scripts/GenerateMap.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGenerator : MonoBehaviour
+public class GenerateMap : MonoBehaviour
 {
     public GameObject Node;
 
     private int MapBREITE = 9;
     private int MapHoehe = 9;
 
+    private List<GameObject> MapNodes = new List<GameObject>();
+    private List<GameObject> EnemyNodes = new List<GameObject>();
+    private List<GameObject> TopTiles = new List<GameObject>();
+    private List<GameObject> BottomTiles = new List<GameObject>();
+    private List<GameObject> MiddleTiles = new List<GameObject>();
+
+
+    private void Start()
+    {
+        MapGenerate();
+    }
     private void MapGenerate()
     {
         for (float z = 0; z < 2 * MapBREITE; z = z + 2)
@@ -17,12 +28,18 @@ public class MapGenerator : MonoBehaviour
             {
                 GameObject newNode = Instantiate(Node);
                 newNode.transform.position = new Vector3(x, 0, z);
+                MapNodes.Add(newNode);
 
             }
         }
+        
     }
-    private void Start()
+    private void PathGeneration()
     {
-        MapGenerate();
+     //    for ( )
+
+
     }
+    
+
 }
