0699a10ae99834e05d02e016d96328f7543008f4
meisterlampeAOV <mr4963@gmx.de>
2020-12-06 20:49:13 +0100

    Mapgenerierung

--- TD_Informatik/Assets/Scripts/GenerateMap.cs
+++ TD_Informatik/Assets/Scripts/GenerateMap.cs
@@ -5,24 +5,51 @@ using UnityEngine;
 public class GenerateMap : MonoBehaviour
 {
     public GameObject Node;
+    public GameObject NodePath;
 
-    private int MapBREITE = 9;
-    private int MapHoehe = 9;
+    [SerializeField] private int MapBREITE = 9;
+    [SerializeField] private int MapHoehe = 9;
 
     private List<GameObject> MapNodes = new List<GameObject>();
     private List<GameObject> EnemyNodes = new List<GameObject>();
-    private List<GameObject> TopTiles = new List<GameObject>();
-    private List<GameObject> BottomTiles = new List<GameObject>();
-    private List<GameObject> MiddleTiles = new List<GameObject>();
+
+    private bool reachedX = false; // temporär
+    private bool reachedZ = false; //temporär
+    private GameObject currentTile; // temporär
+    private int currentIndex; // temporär
+    private int nextIndex; // temporär
+    [SerializeField] private int counter = 0; // temporär
+    [SerializeField] private int counter2 = 0; //temporär
 
 
     private void Start()
     {
         MapGenerate();
     }
+
+    private List<GameObject> getTopTiles() // erstellt die Liste topTiles und fügt alle Tiles am oberen Rand in die Liste von links nach rechts ein, x=[(MapHoehe-1)*2], z={0;2;4;...;(MapBREITE-2)*2;(MapBREITE-1)*2}
+    {
+        List<GameObject> edgeTiles = new List<GameObject>();
+        for (int i = MapBREITE * (MapHoehe - 1); i < MapBREITE * MapHoehe; i++)
+        {
+            edgeTiles.Add(MapNodes[i]);
+        }
+        return edgeTiles;
+    }
+
+    private List<GameObject> getBottomTiles() // erstellt die Liste bottomTiles und fügt alle Tiles am unteren Rand in die Liste von links nach rechts ein, x=0, z={0;2;4;...;(MapBREITE-2)*2;(MapBREITE-1)*2}
+    {
+        List<GameObject> edgeTiles = new List<GameObject>();
+        for (int i = 0; i < MapBREITE; i++)
+        {
+            edgeTiles.Add(MapNodes[i]);
+        }
+        return edgeTiles;
+    }
+
     private void MapGenerate()
     {
-        for (float z = 0; z < 2 * MapBREITE; z = z + 2)
+        for (int z = 0; z < 2 * MapBREITE; z = z + 2)
         {
             for (int x = 0; x < 2 * MapHoehe; x = x + 2)
             {
@@ -32,14 +59,88 @@ public class GenerateMap : MonoBehaviour
 
             }
         }
-        
-    }
-    private void PathGeneration()
-    {
-     //    for ( )
 
+        List<GameObject> topTiles = getTopTiles();
+        List<GameObject> bottomTiles = getBottomTiles();
+
+        GameObject startTile;
+        GameObject endTile;
+
+        int randTop = Random.Range(0, MapBREITE);
+        int randBottom = Random.Range(0, MapBREITE);
+
+        startTile = topTiles[randTop]; //setzt zufälliges Tile vom oberen Rand als Startpunkt fest
+        endTile = bottomTiles[randBottom]; //setzt zufälliges Tile vom unteren Rand als Startpunkt fest
+
+        currentTile = startTile;
+        counter = 0;
+        moveDown();
+        while (reachedX == false)
+        {
+            counter++;
+            
+            if(currentTile.transform.position.x > endTile.transform.position.x)
+            {
+                moveLeft();
+            }
+            else if (currentTile.transform.position.x < endTile.transform.position.x)
+            {
+                moveRight();
+            }
+            else
+            {
+                reachedX = true;
+            }
+        }
+        counter2 = 0;
+        while (reachedZ == false)
+        {
+            counter2++;
+            
+            if(currentTile.transform.position.z > endTile.transform.position.z)
+            {
+                moveDown();
+            }
+            else
+            {
+                reachedZ = true;
+            }
+        }
+
+        EnemyNodes.Add(endTile);
+
+        foreach(GameObject obj in EnemyNodes)
+        {
+            
+            Destroy(obj);
+            GameObject newPathNode = Instantiate(NodePath);
+            newPathNode.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
+            
+        }
 
     }
     
+    private void moveDown() //temporär
+    {
+        EnemyNodes.Add(currentTile);
+        currentIndex = MapNodes.IndexOf(currentTile);
+        nextIndex = currentIndex - MapHoehe;
+        currentTile = MapNodes[nextIndex];
+    }
+    
+    private void moveLeft() //temporär
+    {
+        EnemyNodes.Add(currentTile);
+        currentIndex = MapNodes.IndexOf(currentTile);
+        nextIndex = currentIndex-1;
+        currentTile = MapNodes[nextIndex];
+    }
+    private void moveRight() //temporär
+    {
+        EnemyNodes.Add(currentTile);
+        currentIndex = MapNodes.IndexOf(currentTile);
+        nextIndex = currentIndex+1;
+        currentTile = MapNodes[nextIndex];
+    }
 
 }
