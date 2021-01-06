fc51525ad26c9308764f459b703960379e48750a
Snifif <66800395+Snifif@users.noreply.github.com>
2021-01-06 22:51:58 +0100

    Enemy

--- TD_Informatik/Assets/Scripts/EnemyBehavior.cs
+++ TD_Informatik/Assets/Scripts/EnemyBehavior.cs
@@ -5,26 +5,69 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     public GameObject Enemy;
-    public static List<GameObject> EnemyCount = new List<GameObject>();    
+    public static List<GameObject> Enemies = new List<GameObject>();    
     
     public float Health;
     public float Damage;
     public float Speed;
     public float KillReward;
+    public int Counter = 1;
+    private void Start()
+    {
+
+        InitializeEnemy();
+    }
+
+    private void Update()
+    {
+        MoveEnemy();
+    }
 
     private void InitializeEnemy()
     {
         GameObject newEnemy = Instantiate(Enemy);
         GameObject NodeCoordinates = GenerateMap.PathNodes[0];
+        newEnemy.transform.position = new Vector3(2, 1, 5);
         float x = NodeCoordinates.transform.position.x;
         float z = NodeCoordinates.transform.position.z;
-        newEnemy.transform.position = new Vector3(x, 1, z);
 
+        Enemies.Add(newEnemy);
     }
 
     private void MoveEnemy()
     {
+        int enemycount = Enemies.Count;
+        for (int i = 0; i <= enemycount; i++ ) // für jeden Gegner bewegen
+        {
+            GameObject nextPath = GenerateMap.PathNodes[Counter];
+            float x = nextPath.transform.position.x - Enemies[i].transform.position.x;
+            float z = nextPath.transform.position.z - Enemies[i].transform.position.z;
+            
+            if (x> 0.01)
+            {
+                x = nextPath.transform.position.x - Enemies[i].transform.position.x;
+                Enemies[i].transform.position = Vector3.left;
+            }
+            if (z> 0.01)
+            {
+                z = nextPath.transform.position.z - Enemies[i].transform.position.z;
+                Enemies[i].transform.position = Vector3.up;
 
+            }
+            if (x < 0.01)
+            {
+                x = nextPath.transform.position.x - Enemies[i].transform.position.x;
+                Enemies[i].transform.position = Vector3.right;
+            }
+            if (z < 0.01)
+            {
+                z = nextPath.transform.position.z - Enemies[i].transform.position.z;
+                Enemies[i].transform.position = Vector3.down;
+
+            }
+        }
+
+        Counter++;
 
     }
 
---
+++ TD_Informatik/Assets/Scripts/EnemyBehavior2.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using UnityEngine;
+
+public class EnemyBehavior2 : MonoBehaviour
+{
+    private GameObject TargetNode;
+    [SerializeField]
+    private float Health;
+    [SerializeField]
+    private float Damage;
+    [SerializeField]
+    private float Speed;
+    [SerializeField]
+    private float KillReward;
+
+
+    private float Distancex;
+    private float Distancez;
+
+    public Vector3 TargetZPos;
+    public Vector3 TargetXPos;
+
+    void Start()
+    {
+        InitializeEnemy();
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTargetNode();
+        MoveEnemy();
+    }
+
+    private void InitializeEnemy()
+    {
+        TargetNode = GenerateMap.PathNodes[0];
+    }
+
+    private void MoveEnemy()
+    {
+        
+        if( Distancez >= 0.01)  //in z bewegen
+        {
+            TargetZPos = new Vector3(transform.position.x, transform.position.y + 0.3f, TargetNode.transform.position.z);
+
+            transform.position = Vector3.MoveTowards(transform.position, TargetZPos , Speed * Time.deltaTime);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, TargetNode.transform.position, Speed * Time.deltaTime);
+
+
+        if (Distancex >= 0.01)  //in x bewegen
+        {
+            TargetXPos = new Vector3(TargetNode.transform.position.x, transform.position.y + 0.3f, transform.position.z );
+
+            transform.position = Vector3.MoveTowards(transform.position, TargetXPos, Speed * Time.deltaTime);
+        }
+
+    }
+
+    private void CheckTargetNode()
+    {
+        Distancex = Mathf.Abs(transform.position.x - TargetNode.transform.position.x);
+        Distancez = Mathf.Abs(transform.position.z - TargetNode.transform.position.z);
+
+        if ( TargetNode != null && TargetNode != GenerateMap.endTile )
+        {
+            
+            if (Distancex <= 0.01f && Distancez <= 0.01f)
+            {
+                int NextTargetNode = GenerateMap.PathNodes.IndexOf(TargetNode) + 1;
+                TargetNode = GenerateMap.PathNodes[NextTargetNode];
+
+
+            }
+
+        }
+        if (transform.position == GenerateMap.endTile.transform.position)
+        {
+            die();
+        }
+
+    }
+
+    private void die()
+    {
+        Destroy(transform.gameObject);
+    }
+}
