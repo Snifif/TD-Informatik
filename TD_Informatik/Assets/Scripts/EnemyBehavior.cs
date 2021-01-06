27d8cabb9916ccf367c87d93863e0d24fcd94197
Snifif <66800395+Snifif@users.noreply.github.com>
2021-01-06 17:31:18 +0100

    test

---
+++ TD_Informatik/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public GameObject Enemy;
+    public static List<GameObject> EnemyCount = new List<GameObject>();    
+    
+    public float Health;
+    public float Damage;
+    public float Speed;
+    public float KillReward;
+
+    private void InitializeEnemy()
+    {
+        GameObject newEnemy = Instantiate(Enemy);
+        GameObject NodeCoordinates = GenerateMap.PathNodes[0];
+        float x = NodeCoordinates.transform.position.x;
+        float z = NodeCoordinates.transform.position.z;
+        newEnemy.transform.position = new Vector3(x, 1, z);
+
+    }
+
+    private void MoveEnemy()
+    {
+
+
+    }
+
+
+
+}
