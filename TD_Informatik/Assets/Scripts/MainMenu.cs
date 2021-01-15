7a377cc500cb28d7877ff5c0a41288366e4933bd
Snifif <66800395+Snifif@users.noreply.github.com>
2021-01-15 15:24:59 +0100

    MainMenu

---
+++ TD_Informatik/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartPressedScript()
+    {
+        SceneManager.LoadScene("SampleScene");     // Ladet SampleScene 
+    }
+
+    public void QuitScript()
+    {
+        Application.Quit();
+    }
+}
