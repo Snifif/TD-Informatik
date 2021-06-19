76ceced7f0bd978ba3efc057fb254a6f073378af
meisterlampeAOV <mr4963@gmx.de>
2021-06-19 16:49:27 +0200

    Turret Stacking Patch
    Turrets können jetzt nicht mehr gestackt werden. Des Weiteren werden sie als GameObject ConnectedTurret bei der jeweiligen Node abgespeichert.

--- TD_Informatik/Assets/Scripts/NodeScript.cs
+++ TD_Informatik/Assets/Scripts/NodeScript.cs
@@ -6,7 +6,13 @@ public class NodeScript : MonoBehaviour
 {
     public GameObject LaserTower;
     public GameObject TurretTower;
- 
+    bool towerPlaced = false; // eventuell unnötig
+    private GameObject ConnectedTurret;
+
+    private void Start()
+    {
+        towerPlaced = false;
+    }
 
     void Update()
     {
@@ -15,17 +21,19 @@ public class NodeScript : MonoBehaviour
 
     private void OnMouseDown()  // Spawnen vom gewünschten Tower, wenn auf ein Node gedrückt wird
     {
-        {     
+        if(ConnectedTurret==null){     
             if (GameMenu.LT == true && Money.money >= 140)   
             {
-                Instantiate(LaserTower, new Vector3(gameObject.transform.position.x, 0.8f, gameObject.transform.position.z), Quaternion.identity);
+                ConnectedTurret = Instantiate(LaserTower, new Vector3(gameObject.transform.position.x, 0.8f, gameObject.transform.position.z), Quaternion.identity);
                 Money.money = Money.money - 140;
+                towerPlaced = true;
             }
 
             if (GameMenu.TT == true && Money.money >=20)
             {
-                Instantiate(TurretTower, new Vector3(gameObject.transform.position.x, 0.8f, gameObject.transform.position.z), Quaternion.identity);
+                ConnectedTurret = Instantiate(TurretTower, new Vector3(gameObject.transform.position.x, 0.8f, gameObject.transform.position.z), Quaternion.identity);
                 Money.money = Money.money - 20;
+                towerPlaced = true;
             }
             Money.UpdateBalance();
             
